6476af72d1269c2d57659aee0151f574bc712169
vikassinghma <vikas.singh@mediaagility.com>
2019-10-18 14:52:43 +0530

    Updates

---
+++ precache-manifest.21ce8bd4843f6a5cfd56aab974a91d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea1b38d4104f4c5a9dc3bf14abe737b",
+    "url": "/csv-editor/index.html"
+  },
+  {
+    "revision": "3ffac61ec09f05e5b10f",
+    "url": "/csv-editor/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3c68e7f25c50d9a29fc0",
+    "url": "/csv-editor/static/css/main.ff029e53.chunk.css"
+  },
+  {
+    "revision": "3ffac61ec09f05e5b10f",
+    "url": "/csv-editor/static/js/2.93d00165.chunk.js"
+  },
+  {
+    "revision": "3c68e7f25c50d9a29fc0",
+    "url": "/csv-editor/static/js/main.08807574.chunk.js"
+  },
+  {
+    "revision": "50ef5b7db990d5b2d77f",
+    "url": "/csv-editor/static/js/runtime-main.8195426f.js"
+  }
+]);
