11b3c78b115836524268c440fc42380d902486ad
vikassinghma <vikas.singh@mediaagility.com>
2019-10-18 16:30:12 +0530

    Updates

--- precache-manifest.f7524068fb486de723f1b4924b5b88bc.js
+++ precache-manifest.f7524068fb486de723f1b4924b5b88bc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9baf98529418b9a477908a3c55c8ea6",
+    "revision": "c4a17f75211f6557f78d5a1c82f5d795",
     "url": "/csv-editor/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/csv-editor/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "e953906054e95733e382",
+    "revision": "5b5896be0feb61c668dd",
     "url": "/csv-editor/static/css/main.ff029e53.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/csv-editor/static/js/2.d89d6317.chunk.js"
   },
   {
-    "revision": "e953906054e95733e382",
-    "url": "/csv-editor/static/js/main.85de456f.chunk.js"
+    "revision": "5b5896be0feb61c668dd",
+    "url": "/csv-editor/static/js/main.93a3bce8.chunk.js"
   },
   {
     "revision": "50ef5b7db990d5b2d77f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/csv-editor/precache-manifest.048b25c84b24c73bca091369d1ee6288.js"
+  "/csv-editor/precache-manifest.f7524068fb486de723f1b4924b5b88bc.js"
 );
 
 self.addEventListener('message', (event) => {
